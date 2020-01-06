015afb59b04bd07922b6bd1c54d9d8a606045cfd
shayan <shayanadc@gmail.com>
2020-01-06 15:44:02 +0330

    first commit

---
+++ EmailTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress()
+    {
+        $this->assertFalse(true);
+    }
+}
